fix bug with wrong units

rnn.py
@@ -3,24 +3,21 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 
 # Generate dummy data
-x_train = np.random.random((1000, 20))
-y_train = np.random.randint(2, size=(1000, 1))
-x_test = np.random.random((100, 20))
-y_test = np.random.randint(2, size=(100, 1))
+x = np.random.randint(0, 2, size=(1000, 16))
+y = [n[0] for n in x]
+y = np.array(y)
+x_train = x[:900]
+y_train = y[:900]
+x_test = x[900:]
+y_test = y[900:]
 
 model = Sequential()
-model.add(Dense(64, input_dim=20, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(1, input_dim=16))
 
 model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train,
-          epochs=20,
-          batch_size=128)
-score = model.evaluate(x_test, y_test, batch_size=128)
+model.fit(x_train, y_train, epochs=75)
+score = model.evaluate(x_test, y_test)
 print(score)